7ec0db8ff2be6967def559a7178a4eb414a1f6ab
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-29 17:54:51 -0800

    Left off on activity 'User List'.

--- model.py
+++ model.py
@@ -49,10 +49,25 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     score = db.Column(db.Integer, nullable=False)
 
+    user = db.relationship("User",
+                           backref=db.backref("ratings",
+                                              order_by=rating_id))
+
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings",
+                                               order_by=rating_id))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Rating rating_id={} movie_id={} user_id={} score={}>".format(self.rating_id,
+                                                                              self.movie_id,
+                                                                              self.user_id,
+                                                                              self.score)
 
 ##############################################################################
 # Helper functions
